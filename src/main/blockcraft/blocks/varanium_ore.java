b9d435b243159f4eea1b46a31bd5996f8020b6bc
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-10-30 15:55:43 +0900

    Update varanium_ore.java

--- src/main/blockcraft/blocks/varanium_ore.java
+++ src/main/blockcraft/blocks/varanium_ore.java
@@ -1,37 +1,19 @@
-package com.example.mod.blocks;
+package blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.SoundType;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.registries.ObjectHolder;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
 
 public class VaraniumOreBlock extends Block {
-
-    @ObjectHolder("modid:varanium_ore")
-    public static final Block VARANIUM_ORE = null;
+    public static final Block VARANIUM_ORE = new VaraniumOreBlock();
 
     public VaraniumOreBlock() {
-        super(AbstractBlock.Properties.of(Material.STONE)
-            .strength(3.0f, 3.0f)
-            .sound(SoundType.STONE));
+        super(Block.Properties.of(Material.STONE).strength(3.0F, 3.0F));
     }
 
-    public static void registerBlock(final RegistryEvent.Register<Block> event) {
-        event.getRegistry().registerAll(
-            new VaraniumOreBlock().setRegistryName(new ResourceLocation("modid", "varanium_ore"))
-        );
-    }
-
-    public static void registerBlockItem(final RegistryEvent.Register<Item> event) {
-        event.getRegistry().registerAll(
-            new BlockItem(VARANIUM_ORE, new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS))
-                .setRegistryName(new ResourceLocation("modid", "varanium_ore"))
-        );
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
     }
 }

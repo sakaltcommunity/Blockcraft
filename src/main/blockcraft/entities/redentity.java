e202ef1144fb657931e5d9534d0d776693a83263
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-10-30 16:14:28 +0900

    Update redentity.java

--- src/main/blockcraft/entities/redentity.java
+++ src/main/blockcraft/entities/redentity.java
@@ -1,19 +1,22 @@
-package entities;
+package blockcraft.entities;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.RandomWalkingGoal;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
 import net.minecraft.entity.monster.ZombieEntity;
-import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.world.World;
 
-public class RedEntity extends ZombieEntity {
+public class redentity extends ZombieEntity {
 
-    public RedEntity(EntityType<? extends ZombieEntity> type, World world) {
+    public redentity(EntityType<? extends ZombieEntity> type, World world) {
         super(type, world);
         this.goalSelector.addGoal(0, new MeleeAttackGoal(this, 1.0D, true));
         this.goalSelector.addGoal(1, new RandomWalkingGoal(this, 1.0D));
-        this.goalSelector.addGoal(2, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.addGoal(2, new LookAtGoal(this, net.minecraft.entity.player.PlayerEntity.class, 8.0F));
         this.goalSelector.addGoal(3, new LookRandomlyGoal(this));
     }
 
